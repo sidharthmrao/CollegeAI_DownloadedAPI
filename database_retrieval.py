05f0564e0075079c890b82bf92326ce868eb409e
Sidharth Rao <sidharthmrao@gmail.com>
2022-08-02 23:39:06 -0500

    Configured everything so that now you can use the csv database with database_retrieval.py

--- database_retrieval.py
+++ database_retrieval.py
@@ -1,7 +1,29 @@
 import csv
+import yaml
 
+settings = yaml.load(open('config.yaml'), Loader=yaml.FullLoader)
+colleges = list(settings["colleges"])
+traits_list = list(settings["traits"])
 
+input_path = settings["input_path"]
+output_path = settings["output_path"]
 
-f = csv.reader(open('college_info.csv', 'r'))
-for row in f:
-    print(row[1])
+input_file = csv.DictReader(open('college_info.csv', 'r'))
+output_file = csv.writer(open('output_data.csv', 'w'))
+
+output_file.writerow(["college"]+traits_list)
+
+for row in input_file:
+    if row["name"] in colleges:
+        write_row = [row["name"]]
+
+        for trait in traits_list:
+            try:
+                write_row.append(row[trait])
+            except KeyError:
+                print("Could not find key: " + trait)
+                write_row.append("")
+
+        output_file.writerow(write_row)
+
+print(f"Okay all the stuff is saved in {output_path}")
